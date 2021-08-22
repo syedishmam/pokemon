b3d110a4aa3f5f813859873c2082a24b9685782a
Syed Ishmam <syedishmam17@gmail.com>
2021-08-22 15:08:21 -0400

    Create pokemon constructor

--- src/pokemon/Pokemon.java
+++ src/pokemon/Pokemon.java
@@ -1,5 +1,16 @@
 package pokemon;
 
 public abstract class Pokemon {
+	
+	private int atkPts;
+	private int hitPts;
+	
+	public Pokemon(int atkPts, int hitPts) {
+		//Assuming that all pokemon must have a minimum damage and minimum health of 1
+		if(atkPts < 1) this.atkPts = 1;
+		if(hitPts < 1) this.hitPts = 1;
+		this.atkPts = atkPts;
+		this.hitPts = hitPts;
+	}
 
 }
